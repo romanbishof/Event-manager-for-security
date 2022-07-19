1f49e4cf9f6c07611daf949aa148b3bce383e467
Roman Bishof <roman@octopus-app.com>
2022-07-19 17:37:32 +0300

    added indication for selected Jetty

--- ui/src/components/Sections/Section/Section.jsx
+++ ui/src/components/Sections/Section/Section.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   setCoordinatesJetty,
   setDevicesJetty,
@@ -8,15 +8,24 @@ import {
 import "./Section.css";
 
 function Section({ jetty, coordinates, sectionId }) {
+  const state = useSelector((state) => state.ISMS);
   const dispatch = useDispatch();
+  // const [selectedJetty, setSeletedJetty] = useState(null);
+
   const handleJettyClick = () => {
     dispatch(setCoordinatesJetty(coordinates));
     dispatch(setDevicesJetty(jetty));
     dispatch(setSectionId(sectionId));
   };
 
+  useEffect(() => {}, [state.SectionId]);
+
   return (
-    <div className="Section">
+    <div
+      className={`Section ${
+        state.SectionId === sectionId ? "Section__selected" : ""
+      }`}
+    >
       <button onClick={handleJettyClick}>{jetty}</button>
     </div>
   );
